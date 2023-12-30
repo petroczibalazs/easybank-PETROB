160d4008a000b40cdef78e99e15f6a39b7c6032d
Balázs Miklós Petróczi <petroczibalazs@gmail.com>
2023-12-30 11:03:19 +0100

    add page cover for Articles and HTML for Footer
    Added all the code to show individual images in real size as the user clicks on each and the raw HTML for the footer section.
    
    >>> PROJECT :   [TYPE PROJECT NAME HERE]

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,19 @@
-console.log("Hello from script.js file!");
+
+const gallery_cover = document.querySelector('.cover-gallery');
+const galleryImg = gallery_cover.querySelector('img');
+const gallery_button = gallery_cover.querySelector('button');
+
+gallery_button.addEventListener('click', () => gallery_cover.classList.remove('show'));
+
+
+document.querySelector('.articles').addEventListener('click', handleGalleryClick);
+
+function handleGalleryClick(event){
+
+  const target = event.target;
+
+  if(target.nodeName !='IMG') return;
+  galleryImg.setAttribute('src', target.src);
+  gallery_cover.classList.add('show');
+}
+

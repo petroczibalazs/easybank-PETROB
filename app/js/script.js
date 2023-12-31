a6a7d0ff7a8e6d58d615aeed5aec47d5d3bb5f83
Balázs Miklós Petróczi <petroczibalazs@gmail.com>
2023-12-31 22:05:45 +0100

    All nice and dandy but the header is missing
    --- [TYPE DETAILS ABOUT THE MESSAGE HERE]
    
    >>> PROJECT :   [TYPE PROJECT NAME HERE]

--- app/js/script.js
+++ app/js/script.js
@@ -2,6 +2,7 @@
 const gallery_cover = document.querySelector('.cover-gallery');
 const galleryImg = gallery_cover.querySelector('img');
 const gallery_button = gallery_cover.querySelector('button');
+const body = document.body;
 
 gallery_button.addEventListener('click', () => gallery_cover.classList.remove('show'));
 
@@ -9,6 +10,11 @@ gallery_button.addEventListener('click', () => gallery_cover.classList.remove('s
 document.querySelector('.articles').addEventListener('click', handleGalleryClick);
 
 function handleGalleryClick(event){
+
+
+  let viewPortWidth = body.offsetWidth;
+
+  if(viewPortWidth < 700) return;
 
   const target = event.target;
 

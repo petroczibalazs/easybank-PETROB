cb60e7575652bce854429cb32d7b0594c40e0adb
Balázs Miklós Petróczi <petroczibalazs@gmail.com>
2024-01-01 16:27:27 +0100

    clean - up items-item parts
    --- [TYPE DETAILS ABOUT THE MESSAGE HERE]
    
    >>> PROJECT :   [TYPE PROJECT NAME HERE]

--- app/js/script.js
+++ app/js/script.js
@@ -2,11 +2,25 @@
 const gallery_cover = document.querySelector('.cover-gallery');
 const galleryImg = gallery_cover.querySelector('img');
 const gallery_button = gallery_cover.querySelector('button');
+
 const body = document.body;
+const list  = body.classList;
+const hamburger = document.querySelector('.hamburger');
+
+
+/* event listener for the hamburger menu in the header element*/
+
+hamburger.addEventListener('click', (e) => {
+
+  hamburger.classList.toggle('open');
+  list.toggle('active');
+  (list.contains('active')? list.remove('inactive') : list.add('inactive'));
+});
 
 gallery_button.addEventListener('click', () => gallery_cover.classList.remove('show'));
 
 
+/* event listener function attached to the images in the articles section */
 document.querySelector('.articles').addEventListener('click', handleGalleryClick);
 
 function handleGalleryClick(event){
@@ -22,4 +36,10 @@ function handleGalleryClick(event){
   galleryImg.setAttribute('src', target.src);
   gallery_cover.classList.add('show');
 }
+
+
+
+
+
+
 
